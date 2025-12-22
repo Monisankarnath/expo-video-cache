4b1d8166f06c84e7d8471265f7d04955b579a1eb
Monisankar Nath <monisankar.nath161998@gmail.com>
2025-12-22 16:12:58 +0530

    chore: update module arguments

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import ExpoVideoCacheModule from "./ExpoVideoCacheModule";
 
-export function startServer(port: number): Promise<void> {
-  return ExpoVideoCacheModule.startServer(port);
+export function startServer(
+  port?: number,
+  maxCacheSize?: number
+): Promise<void> {
+  return ExpoVideoCacheModule.startServer(port, maxCacheSize);
 }
 
-export function convertUrl(url: string, port: number): string {
-  return ExpoVideoCacheModule.convertUrl(url, port);
+export function convertUrl(url: string, isCacheable?: boolean): string {
+  return ExpoVideoCacheModule.convertUrl(url, isCacheable);
 }
